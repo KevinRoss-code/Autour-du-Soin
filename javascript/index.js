93da15ab5053430946f31dcb394dd15d47ab8ee1
KevinRoss-code <kevin.rossignol37@yahoo.com>
2023-06-09 22:06:50 +0200

    ajout vite fait

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+window.alert(
+  "Nous tenons à vous informer que notre site internet est actuellement en cours de refonte afin d'améliorer votre expérience en ligne. Nous comprenons que cela puisse causer une gêne temporaire, et nous nous excusons sincèrement pour cela."
+);
