330de5308b28a7565d5312f40819f8ccc578c6a0
KevinRoss-code <kevin.rossignol37@yahoo.com>
2023-09-12 00:11:08 +0200

    front de la page d'accueil

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Sélectionnez l'élément contenant votre texte
+// const scrollText = document.querySelectorAll(".scroll-text");
+
+// Fonction pour gérer l'effet de fondu en fonction de la position de défilement
+// function handleScroll() {
+// Obtenez la position de défilement actuelle de la page
+//   const scrollPosition = window.scrollY;
+
+// Obtenez la position verticale de l'élément par rapport au haut de la page
+//   const elementPosition = scrollText.offsetTop;
+
+// Définissez l'opacité en fonction de la position de l'élément par rapport à la position de défilement
+//   if (scrollPosition > elementPosition - window.innerHeight / 2) {
+//     scrollText.style.opacity = 1;
+//   } else {
+//     scrollText.style.opacity = 0;
+//   }
+// }
+
+// Écoutez l'événement de défilement de la page et appelez la fonction de gestion de l'effet de fondu
+// window.addEventListener("scroll", handleScroll);
+
+// Appelez la fonction une fois au chargement de la page pour gérer l'effet initial
+// handleScroll();
--- js/process_show_partenair.js
+++ js/process_show_partenair.js
@@ -3,56 +3,66 @@ function getPartenair() {
     .post("../serveur/process_show_partenair.php")
     .then((response) => {
       const partenaires = response.data;
-
-      // Assuming you have an element with the class name to hold all articles
       const partenairContainer = document.querySelector(".containerPartenair");
-
-      // Clear the content of the articles container before adding new articles
       partenairContainer.innerHTML = "";
 
       partenaires.forEach((partenaire) => {
-        // Create elements to display article information
-        const partenaireElement = document.createElement("div");
-        partenaireElement.className = "partenaire row";
-
-        const namePElement = document.createElement("div");
-        namePElement.className = "nameP col-2";
-        namePElement.innerHTML = partenaire.name;
-
-        const siteWebElement = document.createElement("div");
-        siteWebElement.className = "siteWeb col-3";
-        siteWebElement.innerHTML = partenaire.site_web;
-
-        const adresseElement = document.createElement("div");
-        adresseElement.className = "adresse col-2";
-        adresseElement.innerHTML = partenaire.adresse;
-
-        const imageDiv = document.createElement("div");
-        imageDiv.className = "imageP col-3";
-
         if (partenaire.image) {
+          const titleElement = document.createElement("div");
+          titleElement.className = "image-title"; // Ajoutez une classe CSS pour le style
+          titleElement.textContent = partenaire.name;
+
+          const imageLink = document.createElement("a");
+          imageLink.href = partenaire.site_web; // Définissez l'URL de redirection ici
+          imageLink.target = "_blank"; // Ouvre le lien dans une nouvelle fenêtre/onglet
+          imageLink.className = "image-container";
+
           const imagePElement = document.createElement("img");
           imagePElement.src = partenaire.image;
-          imageDiv.appendChild(imagePElement);
+
+          imageLink.appendChild(titleElement);
+          imageLink.appendChild(imagePElement);
+          partenairContainer.appendChild(imageLink);
         } else {
-          // If there's no image, you can display a placeholder or handle it as needed
           imageDiv.innerHTML = "No Image Available";
         }
-
-        // Append the elements to the article element
-        partenaireElement.appendChild(namePElement);
-        partenaireElement.appendChild(siteWebElement);
-        partenaireElement.appendChild(adresseElement);
-        partenaireElement.appendChild(imageDiv);
-
-        // Append the article element to the articles container
-        partenairContainer.appendChild(partenaireElement);
       });
+
+      // Appeler la fonction pour démarrer le diaporama une fois que les images ont été chargées.
+      startSlideshow();
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-// Call the function to get the articles when needed, for example, when the page loads
+// Fonction pour gérer le diaporama
+function startSlideshow() {
+  const images = document.querySelectorAll(
+    ".containerPartenair .image-container"
+  );
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.style.display = "block";
+      } else {
+        image.style.display = "none";
+      }
+    });
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  // Afficher la première image
+  showImage(currentIndex);
+
+  // Définir l'intervalle de changement d'image (par exemple, toutes les 3 secondes)
+  setInterval(nextImage, 5000);
+}
+
 getPartenair();
