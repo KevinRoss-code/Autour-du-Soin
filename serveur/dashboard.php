56b94b94a364ec81a7086957a035a90814c658fc
KevinRoss-code <kevin.rossignol37@yahoo.com>
2024-03-25 23:55:56 +0100

    mise en place des tables pour futurs modif

--- serveur/dashboard.php
+++ serveur/dashboard.php
@@ -16,6 +16,9 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     <meta charset="UTF-8">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+    <link type="text/css" rel="stylesheet" href="../css/datatable.css" />
     <title>Dashboard</title>
     <style>
         /* Style pour cacher les formulaires et les tables */
@@ -131,15 +134,233 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         <input type="button" value="Ajouter" id="sendFormPartenair" />
     </form>
 
+    <h2 id="update-link">Modifier</h2>
+    <table id="tableUpdateMassage" style="width: 100%">
+        <h3>Massage</h3>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Durée</th>
+                <th>Prix</th>
+                <th>Image</th>
+                <th>Bouton modfi.</th>
+                <!-- Ajoutez d'autres colonnes si nécessaire -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données seront insérées ici dynamiquement -->
+        </tbody>
+    </table>
+    <table id="tableUpdateMaquillage" style="width: 100%">
+        <h3>Maquillage</h3>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Durée</th>
+                <th>Prix</th>
+                <th>Bouton modfi.</th>
+                <!-- Ajoutez d'autres colonnes si nécessaire -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données seront insérées ici dynamiquement -->
+        </tbody>
+    </table>
+    <table id="tableUpdateEpilation" style="width: 100%">
+        <h3>Epilation</h3>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Durée</th>
+                <th>Prix</th>
+                <th>Bouton modfi.</th>
+                <!-- Ajoutez d'autres colonnes si nécessaire -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données seront insérées ici dynamiquement -->
+        </tbody>
+    </table>
+    <table id="tableUpdateSoinVisage" style="width: 100%">
+        <h3>Soin du visage</h3>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Durée</th>
+                <th>Prix</th>
+                <th>Bouton modfi.</th>
+                <!-- Ajoutez d'autres colonnes si nécessaire -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données seront insérées ici dynamiquement -->
+        </tbody>
+    </table>
+    <table id="tableUpdateOngle" style="width: 100%">
+        <h3>Ongles</h3>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Durée</th>
+                <th>Prix</th>
+                <th>Bouton modfi.</th>
+                <!-- Ajoutez d'autres colonnes si nécessaire -->
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Les données seront insérées ici dynamiquement -->
+        </tbody>
+    </table>
+
     <!-- JavaScript pour gérer l'affichage/masquage des formulaires -->
     <script>
+        $(document).ready(function() {
+            $('table').not(':first').hide();
+
+            // Gérer le clic sur l'élément h2
+            $('#update-link').click(function() {
+                // Si la première table est visible, les cacher toutes
+                if ($('table:first').is(':visible')) {
+                    $('table').hide();
+                } else { // Sinon, afficher toutes les tables
+                    $('table').show();
+                }
+            });
+
+            $('#tableUpdateMassage').DataTable({
+                "ajax": {
+                    "url": "process_show_article.php", // Spécifiez l'URL de votre script PHP ici
+                    "dataSrc": "" // Cela indique à DataTables que les données sont directement disponibles dans l'objet JSON
+                },
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "massage_description"
+                    },
+                    {
+                        "data": "duree_massage"
+                    },
+                    {
+                        "data": "prix"
+                    },
+                    {
+                        "data": "image_path"
+                    },
+                    {
+                        "render": function(data, type, row) {
+                            return '<button class="btn-show-first-column">Afficher</button>';
+                        }
+                    }
+                    // Ajoutez d'autres colonnes si nécessaire
+                ]
+            });
+            $('#tableUpdateMaquillage').DataTable({
+                "ajax": {
+                    "url": "process_show_makeup.php", // Spécifiez l'URL de votre script PHP ici
+                    "dataSrc": "" // Cela indique à DataTables que les données sont directement disponibles dans l'objet JSON
+                },
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "duree"
+                    },
+                    {
+                        "data": "prix"
+                    },
+                    {
+                        "render": function(data, type, row) {
+                            return '<button class="btn-show-first-column">Afficher</button>';
+                        }
+                    }
+                    // Ajoutez d'autres colonnes si nécessaire
+                ]
+            });
+            $('#tableUpdateEpilation').DataTable({
+                "ajax": {
+                    "url": "process_show_hair_removal.php", // Spécifiez l'URL de votre script PHP ici
+                    "dataSrc": "" // Cela indique à DataTables que les données sont directement disponibles dans l'objet JSON
+                },
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "duree"
+                    },
+                    {
+                        "data": "prix"
+                    },
+                    {
+                        "render": function(data, type, row) {
+                            return '<button class="btn-show-first-column">Afficher</button>';
+                        }
+                    }
+                    // Ajoutez d'autres colonnes si nécessaire
+                ]
+            });
+            $('#tableUpdateSoinVisage').DataTable({
+                "ajax": {
+                    "url": "process_show_care.php", // Spécifiez l'URL de votre script PHP ici
+                    "dataSrc": "" // Cela indique à DataTables que les données sont directement disponibles dans l'objet JSON
+                },
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "dure"
+                    },
+                    {
+                        "data": "prix"
+                    },
+                    {
+                        "render": function(data, type, row) {
+                            return '<button class="btn-show-first-column">Afficher</button>';
+                        }
+                    }
+                    // Ajoutez d'autres colonnes si nécessaire
+                ]
+            });
+            $('#tableUpdateOngle').DataTable({
+                "ajax": {
+                    "url": "process_show_nail.php", // Spécifiez l'URL de votre script PHP ici
+                    "dataSrc": "" // Cela indique à DataTables que les données sont directement disponibles dans l'objet JSON
+                },
+                "columns": [{
+                        "data": "name"
+                    },
+                    {
+                        "data": "duree"
+                    },
+                    {
+                        "data": "prix"
+                    },
+                    {
+                        "render": function(data, type, row) {
+                            return '<button class="btn-show-first-column">Afficher</button>';
+                        }
+                    }
+                    // Ajoutez d'autres colonnes si nécessaire
+                ]
+            });
+        });
         // Fonction pour afficher/masquer les formulaires d'ajout
         document.getElementById("ajouter-link").addEventListener("click", function() {
-            var formulaires = document.querySelectorAll(".formulaire"); // Sélectionne tous les formulaires avec la classe "formulaire"
+            let formulaires = document.querySelectorAll(".formulaire"); // Sélectionne tous les formulaires avec la classe "formulaire"
             formulaires.forEach(function(form) { // Parcours chaque formulaire
                 form.classList.toggle("hidden"); // Inverse la classe "hidden" pour afficher ou masquer le formulaire
             });
         });
+
+        // document.getElementById("update-link").addEventListener("click", function() {
+        //     let table = document.getElementById("tableUpdate");
+        //     if (table.style.display === "none") {
+        //         table.style.display = "table";
+        //     } else {
+        //         table.style.display = "none";
+        //     }
+        // });
     </script>
 </body>
 
